bbd73bee8544e2c84594514584f1b92b53d93016
Matias Maira <matiasmaira@Matiass-MacBook-Air.local>
2026-08-11 10:14:09 -0400

    feat: add BOOMBOX executive control center

--- features/operations/components/index.ts
+++ features/operations/components/index.ts
@@ -1,4 +1,5 @@
 export * from "./command-center";
+export * from "./executive-control-center";
 export * from "./live-event-experience";
 export * from "./operations-gate-experience";
 export * from "./preparation-experience";
